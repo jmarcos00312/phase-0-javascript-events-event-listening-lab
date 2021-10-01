957a52f244c2399fea8434df99a3ccdd1bba3a7c
Jeremiah <jeremiahmarcos27@outlook.com>
2021-10-01 12:34:49 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+  const input = document.getElementById("input");
+  function alerted() {
+    alert("IT'S A TRAPP!!");
+  }
+  input.addEventListener("click", addingEventListener);
 }
